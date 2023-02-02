9a9ec306eb7e5fec50c132f520e49d104fd969dc
Nikita Kulichev <longrast.2002@gmail.com>
2023-02-02 17:53:02 +0300

    not completed for desktop

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - FAQ accordion card solution
+
+This is a solution to the [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](images/screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/longrast/FrontendmentorChallenge-4)
+- Live Site URL: [Add live site URL here](https://longrast.github.io/FrontendmentorChallenge-4/)
+
+## My process
+
+### Built with
+
+- SCSS custom properties
+- Flexbox
+- Basic JS
+
+## Author
+
+- Frontend Mentor - [@longrast](https://www.frontendmentor.io/profile/longrast)
+
---
+++ script/script.js
@@ -0,0 +1,90 @@
+function show1(){
+    document.getElementById('v1').style.display ='block';
+    document.getElementById('v2').style.display ='none';
+    document.getElementById('v3').style.display ='none';
+    document.getElementById('v4').style.display ='none';
+    document.getElementById('v5').style.display ='none';
+    document.getElementById('a1').style.transform = 'rotate(180deg)';
+    document.getElementById('a2').style.transform = 'rotate(0deg)';
+    document.getElementById('a3').style.transform = 'rotate(0deg)';
+    document.getElementById('a4').style.transform = 'rotate(0deg)';
+    document.getElementById('a5').style.transform = 'rotate(0deg)';
+    document.getElementById('x1').style.fontWeight = '700';
+    document.getElementById('x2').style.fontWeight = '400';
+    document.getElementById('x3').style.fontWeight = '400';
+    document.getElementById('x4').style.fontWeight = '400';
+    document.getElementById('x5').style.fontWeight = '400';
+
+}
+function show2(){
+    document.getElementById('v1').style.display ='none';
+    document.getElementById('v2').style.display ='block';
+    document.getElementById('v3').style.display ='none';
+    document.getElementById('v4').style.display ='none';
+    document.getElementById('v5').style.display ='none';
+    document.getElementById('a1').style.transform = 'rotate(0deg)';
+    document.getElementById('a2').style.transform = 'rotate(180deg)';
+    document.getElementById('a3').style.transform = 'rotate(0deg)';
+    document.getElementById('a4').style.transform = 'rotate(0deg)';
+    document.getElementById('a5').style.transform = 'rotate(0deg)';
+    document.getElementById('x1').style.fontWeight = '400';
+    document.getElementById('x2').style.fontWeight = '700';
+    document.getElementById('x3').style.fontWeight = '400';
+    document.getElementById('x4').style.fontWeight = '400';
+    document.getElementById('x5').style.fontWeight = '400';
+
+}
+function show3(){
+    document.getElementById('v1').style.display ='none';
+    document.getElementById('v2').style.display ='none';
+    document.getElementById('v3').style.display ='block';
+    document.getElementById('v4').style.display ='none';
+    document.getElementById('v5').style.display ='none';
+    document.getElementById('a1').style.transform = 'rotate(0deg)';
+    document.getElementById('a2').style.transform = 'rotate(0deg)';
+    document.getElementById('a3').style.transform = 'rotate(180deg)';
+    document.getElementById('a4').style.transform = 'rotate(0deg)';
+    document.getElementById('a5').style.transform = 'rotate(0deg)';
+    document.getElementById('x1').style.fontWeight = '400';
+    document.getElementById('x2').style.fontWeight = '400';
+    document.getElementById('x3').style.fontWeight = '700';
+    document.getElementById('x4').style.fontWeight = '400';
+    document.getElementById('x5').style.fontWeight = '400';
+}
+function show4(){
+    document.getElementById('v1').style.display ='none';
+    document.getElementById('v2').style.display ='none';
+    document.getElementById('v3').style.display ='none';
+    document.getElementById('v4').style.display ='block';
+    document.getElementById('v5').style.display ='none';
+    document.getElementById('a1').style.transform = 'rotate(0deg)';
+    document.getElementById('a2').style.transform = 'rotate(0deg)';
+    document.getElementById('a3').style.transform = 'rotate(0deg)';
+    document.getElementById('a4').style.transform = 'rotate(180deg)';
+    document.getElementById('a5').style.transform = 'rotate(0deg)';
+    document.getElementById('x1').style.fontWeight = '400';
+    document.getElementById('x2').style.fontWeight = '400';
+    document.getElementById('x3').style.fontWeight = '400';
+    document.getElementById('x4').style.fontWeight = '700';
+    document.getElementById('x5').style.fontWeight = '400';
+
+}
+function show5(){
+    document.getElementById('v1').style.display ='none';
+    document.getElementById('v2').style.display ='none';
+    document.getElementById('v3').style.display ='none';
+    document.getElementById('v4').style.display ='none';
+    document.getElementById('v5').style.display ='block';
+    document.getElementById('a1').style.transform = 'rotate(0deg)';
+    document.getElementById('a2').style.transform = 'rotate(0deg)';
+    document.getElementById('a3').style.transform = 'rotate(0deg)';
+    document.getElementById('a4').style.transform = 'rotate(0deg)';
+    document.getElementById('a5').style.transform = 'rotate(180deg)';
+    document.getElementById('x1').style.fontWeight = '400';
+    document.getElementById('x2').style.fontWeight = '400';
+    document.getElementById('x1').style.fontWeight = '400';
+    document.getElementById('x2').style.fontWeight = '400';
+    document.getElementById('x3').style.fontWeight = '400';
+    document.getElementById('x4').style.fontWeight = '400';
+    document.getElementById('x5').style.fontWeight = '700';
+}
